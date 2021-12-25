2c8c4a0b59e1d47e56c1966379419b3788463581
thejagenji <54184829+GTheja@users.noreply.github.com>
2021-12-25 15:38:12 +0530

    Add files via upload

---
+++ src/main/java/app/n2/main/authentication/LoginDTO.java
@@ -0,0 +1,19 @@
+package app.n2.main.authentication;
+
+public class LoginDTO {
+    public String email;
+    public String password;
+
+    public LoginDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/app/n2/main/rest/N2NResource.java
@@ -0,0 +1,8 @@
+package app.n2.main.rest;
+
+import javax.ws.rs.Path;
+
+@Path("/")
+public class N2NResource {
+
+}
