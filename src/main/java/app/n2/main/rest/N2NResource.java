57433aa4341b5c30e380e1afd96a755f118404f3
Genji Theja <theja.genji@kristasoft.com>
2021-12-25 15:55:32 +0530

    Added Service and Resource

--- src/main/java/app/n2/main/rest/N2NResource.java
+++ src/main/java/app/n2/main/rest/N2NResource.java
@@ -1,8 +1,29 @@
 package app.n2.main.rest;
 
+import app.n2.main.service.N2NAuthentication;
+
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class N2NResource {
+
+    N2NAuthentication n2NAuthentication = new N2NAuthentication();
+
+    @POST
+    @Path("/login")
+    public Response getLogin(@Context HttpHeaders headers){
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("/register")
+    public Response getRegister(@Context HttpHeaders headers){
+        n2NAuthentication.registerUser();
+        return Response.ok().build();
+    }
 
 }
---
+++ src/main/java/app/n2/main/service/N2NAuthentication.java
@@ -0,0 +1,12 @@
+package app.n2.main.service;
+
+import app.n2.main.authentication.RegisterDTO;
+
+
+public class N2NAuthentication {
+
+    public String registerUser(){
+        RegisterDTO  registerDTO = new RegisterDTO("","","","");
+        return null;
+    }
+}
